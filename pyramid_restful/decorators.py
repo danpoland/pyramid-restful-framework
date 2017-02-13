0e09bcf38471e46133be37739b325e4d775f0313
Dan Poland <danpoland84@gmail.com>
2017-02-13 09:41:44 -0500

    test decorators

---
+++ pyramid_restful/decorators.py
@@ -0,0 +1,35 @@
+__all__ = ['detail_route', 'list_route']
+
+
+def detail_route(methods=None, **kwargs):
+    """
+    Used to mark a method on a ViewSet that should be routed for detail requests.
+    """
+
+    methods = ['get'] if (methods is None) else methods
+
+    def decorator(func):
+        func.bind_to_methods = methods
+        func.detail = True
+        func.kwargs = kwargs
+
+        return func
+
+    return decorator
+
+
+def list_route(methods=None, **kwargs):
+    """
+    Used to mark a method on a ViewSet that should be routed for list requests.
+    """
+
+    methods = ['get'] if (methods is None) else methods
+
+    def decorator(func):
+        func.bind_to_methods = methods
+        func.detail = False
+        func.kwargs = kwargs
+
+        return func
+
+    return decorator
---
+++ tests/test_decorartors.py
@@ -0,0 +1,27 @@
+from unittest import TestCase
+from unittest.mock import MagicMock
+
+from pyramid.config import Configurator
+
+from pyramid_restful.decorators import list_route, detail_route
+
+
+class DecoratorTests(TestCase):
+
+    def test_list_route(self):
+        @list_route(methods=['GET'], url_path='my-list-route')
+        def my_list_route():
+            pass
+
+        assert my_list_route.bind_to_methods == ['GET']
+        assert my_list_route.detail == False
+        assert my_list_route.kwargs == {'url_path': 'my-list-route'}
+
+    def test_detail_route(self):
+        @detail_route(methods=['PUT'], url_path='my-detail-route')
+        def my_detail_route():
+            pass
+
+        assert my_detail_route.bind_to_methods == ['PUT']
+        assert my_detail_route.detail == True
+        assert my_detail_route.kwargs == {'url_path': 'my-detail-route'}
