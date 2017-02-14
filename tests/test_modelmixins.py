76163203a3907e26ab31e7a056d3fd72e9219767
Dan Poland <danpoland84@gmail.com>
2017-02-13 23:27:58 -0500

    clean up

--- tests/test_modelmixins.py
+++ tests/test_modelmixins.py
@@ -37,7 +37,7 @@ class MockAPIView:
         return query
 
     def get_schema(self):
-        def dump(data, *args, many=False, **kwargs):
+        def dump(data, many=False, **kwargs):
             if many:
                 return [{'id': i.id, 'name': i.name} for i in data], ''
 
