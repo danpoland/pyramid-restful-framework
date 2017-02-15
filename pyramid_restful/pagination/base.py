b85d9e6705d59f8b19dfaa1a516379bb8148b1b0
Dan Poland <danpoland84@gmail.com>
2017-02-14 21:41:13 -0500

    pagination tests

--- pyramid_restful/pagination/base.py
+++ pyramid_restful/pagination/base.py
@@ -1,7 +1,7 @@
 class BasePagination:
 
     def paginate_query(self, queryset, request):
-        raise NotImplementedError('paginate_queryset() must be implemented.')
+        raise NotImplementedError('paginate_queryset() must be implemented.')  # pragma: no cover
 
     def get_paginated_response(self, data):
-        raise NotImplementedError('get_paginated_response() must be implemented.')
+        raise NotImplementedError('get_paginated_response() must be implemented.')  # pragma: no cover
--- pyramid_restful/pagination/pagenumber.py
+++ pyramid_restful/pagination/pagenumber.py
@@ -1,5 +1,8 @@
 from collections import OrderedDict, Sequence
+
 from math import ceil
+
+import warnings
 
 import six
 from pyramid.exceptions import HTTPNotFound
@@ -7,7 +10,6 @@ from pyramid.response import Response
 
 from .utilities import remove_query_param, replace_query_param
 from .base import BasePagination
-
 
 __all__ = ['PageNumberPagination']
 
@@ -38,23 +40,22 @@ class EmptyPage(InvalidPage):
     pass
 
 
-class Paginator(object):
-    """
-    Thanks Django!
-    """
+class UnorderedObjectListWarning(RuntimeWarning):
+    pass
 
+
+class Paginator:
     def __init__(self, object_list, per_page, orphans=0, allow_empty_first_page=True):
         self.object_list = object_list
+        self._check_object_list_is_ordered()
         self.per_page = int(per_page)
         self.orphans = int(orphans)
         self.allow_empty_first_page = allow_empty_first_page
-        self._num_pages = self._count = None
 
     def validate_number(self, number):
         """
         Validates the given 1-based page number.
         """
-
         try:
             number = int(number)
         except (TypeError, ValueError):
@@ -75,16 +76,13 @@ class Paginator(object):
         number = self.validate_number(number)
         bottom = (number - 1) * self.per_page
         top = bottom + self.per_page
-
         if top + self.orphans >= self.count:
             top = self.count
-
         return self._get_page(self.object_list[bottom:top], number, self)
 
     def _get_page(self, *args, **kwargs):
         """
         Returns an instance of a single page.
-
         This hook can be used by subclasses to use an alternative to the
         standard :cls:`Page` object.
         """
@@ -95,32 +93,23 @@ class Paginator(object):
         """
         Returns the total number of objects, across all pages.
         """
-
-        if self._count is None:
-            try:
-                self._count = self.object_list.count()
-            except (AttributeError, TypeError):
-                # AttributeError if object_list has no count() method.
-                # TypeError if object_list.count() requires arguments
-                # (i.e. is of type list).
-                self._count = len(self.object_list)
-
-        return self._count
+        try:
+            return self.object_list.count()
+        except (AttributeError, TypeError):
+            # AttributeError if object_list has no count() method.
+            # TypeError if object_list.count() requires arguments
+            # (i.e. is of type list).
+            return len(self.object_list)
 
     @property
     def num_pages(self):
         """
         Returns the total number of pages.
         """
-
-        if self._num_pages is None:
-            if self.count == 0 and not self.allow_empty_first_page:
-                self._num_pages = 0
-            else:
-                hits = max(1, self.count - self.orphans)
-                self._num_pages = int(ceil(hits / float(self.per_page)))
-
-        return self._num_pages
+        if self.count == 0 and not self.allow_empty_first_page:
+            return 0
+        hits = max(1, self.count - self.orphans)
+        return int(ceil(hits / float(self.per_page)))
 
     @property
     def page_range(self):
@@ -128,8 +117,18 @@ class Paginator(object):
         Returns a 1-based range of pages for iterating through within
         a template for loop.
         """
+        return range(1, self.num_pages + 1)
 
-        return six.moves.range(1, self.num_pages + 1)
+    def _check_object_list_is_ordered(self):
+        """
+        Warn if self.object_list is unordered (typically a QuerySet).
+        """
+        if hasattr(self.object_list, 'ordered') and not self.object_list.ordered:
+            warnings.warn(
+                'Pagination may yield inconsistent results with an unordered '
+                'object_list: {!r}'.format(self.object_list),
+                UnorderedObjectListWarning
+            )
 
 
 class Page(Sequence):
@@ -146,7 +145,7 @@ class Page(Sequence):
         return len(self.object_list)
 
     def __getitem__(self, index):
-        if not isinstance(index, (slice,) + six.integer_types):
+        if not isinstance(index, (int, slice)):
             raise TypeError
         # The object_list is converted to a list so that if it was a QuerySet
         # it won't be a database hit per __getitem__.
