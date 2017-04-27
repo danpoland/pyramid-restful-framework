7356ea3efc2af3579b24db9781098430634c2b1b
Dan Poland <danpoland84@gmail.com>
2017-04-26 21:36:44 -0400

    add support in , list filtering to field filter. Also relationhip attribute filtering

--- pyramid_restful/filters.py
+++ pyramid_restful/filters.py
@@ -1,3 +1,6 @@
+from sqlalchemy import or_
+
+
 class BaseFilter:
     """
     Base filter that all filter classes must implement.
@@ -14,24 +17,57 @@ class FieldFilter(BaseFilter):
     """
     Filters a query based on the filter_fields set on the view.
     filter_fields should be a list of SQLAlchemy Model columns.
+    Supports filtering a comma separated list using OR statements.
+    Supports relationship filter using the . path to attribute. WARNING: Every relationship in a . path is joined.
     """
 
     def filter_query(self, request, query, view):
+        if not view.filter_fields or not request.params:
+            return query
+
         filter_list = []
+        querystring_params = self.parse_query_string(request.params)
+        available_fields = list(map(lambda x: '{}.{}'.format(x.parent.class_.__name__, x.name), view.filter_fields))
 
-        if view.filter_fields and request.params:
-            available_fields = list(map(lambda x: x.name, view.filter_fields))
-            querystring_params = self.parse_query_string(request.params)
+        for key, val in querystring_params.items():
+            attrs = key.split('.')  # Every item in the resulting array must be a relationship except for the last
+            related_model = view.model
+            join_models = []
 
-            for key, val in querystring_params.items():
+            for attr in attrs[:-1]:
+                # Loop through all the relationships and build the list of tables that need to be joined
+                # to the query. The final value of related_model will be the model representing the table
+                # with the attribute to be filtered on.
+
+                relationship = getattr(related_model, attr, None)
+
+                if relationship is None:
+                    related_model = None
+                    break
+
+                related_model = relationship.mapper.class_
+                join_models.append(related_model)
+
+            attr = attrs[-1]
+
+            if related_model and hasattr(related_model, attr):
                 try:
-                    i = available_fields.index(key)
+                    i = available_fields.index('{}.{}'.format(related_model.__name__, attr))
                 except ValueError:
                     continue
 
-                filter_list.append(view.filter_fields[i] == val)
+                joined_tables = [mapper.class_ for mapper in query._join_entities]  # accessing protected attribute?
 
-        return query.filter(*filter_list) if filter_list else query
+                for join_model in join_models:
+                    if join_model not in joined_tables:
+                        query = query.join(join_model)
+
+                # If there is only one comparison in or_ it is just AND'd on the query
+                filter_list.append(or_(*[view.filter_fields[i] == v for v in val.split(',')]))
+
+            query = query.filter(*filter_list)
+
+        return query
 
     def parse_query_string(self, params):
         """
