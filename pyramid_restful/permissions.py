a6da1073997ae34afacb495d04df10c7ad1a8435
Dan Poland <danpoland84@gmail.com>
2017-08-03 11:20:33 -0400

    add permission base class

---
+++ pyramid_restful/permissions.py
@@ -0,0 +1,22 @@
+class BasePermission:
+    """
+    All permission classes should inherit from this class.
+
+    Override message to customize the message associated with the exception.
+    """
+
+    message = None
+
+    def has_permission(self, request, view):
+        """
+        Return `True` if permission is granted else false.
+        """
+
+        return True
+
+    def has_object_permission(self, request, view, obj):
+        """
+        Return `True` if permission is granted else false.
+        """
+
+        return True
